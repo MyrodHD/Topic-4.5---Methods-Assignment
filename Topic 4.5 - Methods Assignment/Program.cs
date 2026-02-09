be2764223a99d633b6571a1f8cb6952cab6944fb
MyrodHD <Krysalink@outlook.com>
2026-02-09 12:41:53 -0500

    Add project files.

---
+++ Topic 4.5 - Methods Assignment/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Topic_4._5___Methods_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like to see Art or a Joke?");
+            Console.ReadLine();
+        }
+    }
+}
